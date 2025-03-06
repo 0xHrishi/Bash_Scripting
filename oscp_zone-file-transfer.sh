1d5c9c5be46d7ea0384b09ffb145679b0524b6db
Hrishi Ghosalkar <87890571+RookieRecon@users.noreply.github.com>
2025-03-06 09:59:40 +0530

    Update oscp_zone-file-transfer.sh

--- oscp_zone-file-transfer.sh
+++ oscp_zone-file-transfer.sh
@@ -1,82 +1,67 @@
-#script for oscp exercise for attempting zone file transfer
-#can also automate the script with the help of for loop. 
+#zone file tarnsfer for oscp
 
 #!/bin/bash
 
-echo "Enter the domain name such as new.com"
-read domain 
-
-if [ -z "$domain" ]
-then
-        echo "User input empty"
-else
-        echo "*****************************"
-        echo "Looking for Name Servers" 
-        host -t ns $domain | cut -d " " -f4
-        echo "*****************************"
-        echo "Would you like, zone file transfer, Y or N" 
-        read choice
-        if [ "$choice" == "Y" ] || [ "$choice" == "y" ]
-        then
-                echo "Enter the name server"
-                read name_server
-                echo "***************************"
-                host -l $domain $name_server
-        else
-                echo "Bye for now"
-        fi
-
-fi
-------------------------------------------------------------------------------------------------------------------------------------------------------------
-#!/bin/bash
-
-
-echo "Enter the domain name"
+# user input --> ENter the domain
+echo "Enter the domain name i.e. google.com"
 read domain
 
+function lines {
+        echo "*******************************"
+}
+
+#check user input is empty or the length of the string is equal to zero
 if [ -z "$domain" ]
 then
-        echo "*******************************"
-        echo "Domain field is empty"
+        lines
+        echo "Domain field cannot be empty"
 else
-        echo "*******Looking for DNS servers********"
-        sleep 0.3
         host -t ns $domain &>/dev/null
         if [ $? -eq 0 ]
         then
-                echo "*******Name Servers found*********"
-                sleep 0.3
-                host -t ns $domain | cut -d " " -f4 > ./name-servers
-                sleep 0.3
-                echo "Would you like to do a zone file transfer y/n"
-                read choice
-                if [ "$choice" == "y" ]
+                lines
+                echo "******Name servers found --> $domain"
+                sleep 1
+                host -t ns $domain | cut -d " " -f4 | cut -d "." -f1-3
+                if [ $? -eq 0 ]
                 then
-                        for zone_transfer in $(cat ./name-servers)
-                        do
-                                host -l $domain $zone_transfer &>/dev/null
-                                if [ $? -eq 0 ]
-                                then
-                                        echo "*******$zone_transfer Done********"
-                                        host -l $domain $zone_transfer
+                        host -t ns $domain | cut -d " " -f4 | cut -d "." -f1-3 > ./name_servers
+                        sleep 1
+                         if [ -s ./name_servers ]
+                         then
+                                 lines
+                                 echo "Would you like to attempt zone file tarnsfer Y/y or N/n --> $domain"
+                                 read option
+                                 if [ "$option" == "Y" ] || [ "$option" == "y" ]
+                                 then
+                                         for zone_transfers in $(cat ./name_servers)
+                                         do
+                                                host -l $domain $zone_transfers &>/dev/null
+                                                if [ $? -eq 0 ]
+                                                then
+                                                        lines
+                                                        host -l $domain $zone_transfers
+                                                else
+                                                        lines
+                                                        echo "Zone file tarnsfer failed --> $zone_transfers"
+                                                        continue
+                                                fi
 
-                                else
-                                        echo "*******$zone_transfer failed********"
-                                        sleep 0.5
-                                        continue
-                                fi
-                        done
-                elif [ "$choice" == "n" ]
-                then
-                        echo "*******No zone file tarnsfer conducted as per user request********"
-                else
-                        echo "********Invalid user input***********"
+                                         done
+                                 elif [ "$option" == "N" ] || [ "$option" == "n" ]
+                                 then
+                                         lines
+                                         echo "You chooise to quit, bye"
+                                 else
+                                         lines
+                                         echo "Invalid user input"
+                                 fi
+                         fi
                 fi
         else
-                echo "**********************************"
-                echo "Name servers not found"
+                lines
+                echo "*********Inavlid hostname --> $domain**********"
         fi
 fi
 rm -rf ./1
-rm -rf ./name-servers
-
+rm -rf ./name_servers
