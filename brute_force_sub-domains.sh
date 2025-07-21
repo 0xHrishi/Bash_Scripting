39d697212ca3f9fd707bda43c4f2a5c4b96fe2de
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-07-21 09:51:33 +0530

    Update brute_force_sub-domains.sh

--- brute_force_sub-domains.sh
+++ brute_force_sub-domains.sh
@@ -1,69 +1,70 @@
 #!/bin/bash
-#script --> Brute force to find hostname related to domain 
-
-#user input i.e. domain name and filepath which consits for most common sub domains lists
-echo "Enter the domain name i.e. google.com"
-read domain
-echo "Enter the file path for brute force attack on domain"
-read filepath
 
 function lines {
-        echo "************************************"
+        echo "******************************************"
 }
 
-#user input empty
-if [ -z "$domain" ] || [ -z "$filepath" ]
+echo "Enter the hostname such as google.com"
+read domain
+echo "Enter the file path for brute force"
+read file
+
+if [ -z "$domain" ] || [ -z "$file" ]
 then
-        if [ -n "$domain" ] && [ -z "$filepath" ]
+        if [ -n "$domain" ] && [ -z "$file" ]
         then
                 lines
-                echo "Filepath field is empty"
-        elif [ -z "$domain" ] && [ -n "$filepath" ]
+                echo "File field empty"
+        elif [ -z "$domain" ] && [ -n "$file" ]
         then
                 lines
-                echo "Domain name field is empty"
-        elif [ -z "$domain" ] && [ -z "$filepath" ]
+                echo "Domain name field empty"
+        elif [ -z "$domain" ] && [ -z "$file" ]
         then
                 lines
-                echo "Domain name field is empty"
-                echo "Filepath field is empty"
+                echo "Domain name field empty"
+                echo "File field empty"
         else
                 lines
                 echo "Issues with the user input"
         fi
-
-#user input not empty
-elif [ -n "$domain" ] && [ -n "$filepath" ]
+elif [ -n "$domain" ] && [ -n "$file" ]
 then
-        if [ -f $filepath ]
+        if [ -e $file ]
         then
-                #for loop 
-                for sub_domains in $(cat $filepath)
-                do
-                        host $sub_domains.$domain &>/dev/null
-
-                        if [ $? -eq 0 ]
-                        then
-                                lines
-                                host $sub_domains.$domain | cut -d " " -f1,4
-                                host $sub_domains.$domain | cut -d " " -f1,4 >> ./sub_domains_lists
-                                sleep 0.5
-                        else
-                                lines
-                                echo "$sub_domains.$domain not found"
-                                host $sub_domains.$domain | cut -d " " -f1,4 >> ./sub_domains_lists
-                                continue
-                                sleep 0.5
-                        fi
-                done
-        elif [ -d $filepath ]
-        then
-                lines
-                echo "$filepath --> Its a directory"
-                echo "Nothing can be done"
+                if [ -d $file ]
+                then
+                        lines
+                        echo "$file --> Its a directory"
+                        echo "Nothing can be done"
+                elif [ -s $file ] && [ -f $file ]
+                then
+                        for sub_domains in $(cat $file)
+                        do
+                                host $sub_domains.$domain &>/dev/null
+                                if [ $? -eq 0 ]
+                                then
+                                        lines
+                                        host $sub_domains.$domain
+                                        host $sub_domains.$domain >> sub_domains
+                                        sleep 0.2
+                                        continue
+                                else
+                                        lines
+                                        echo "$sub_domains.$domain --> NO PUBLIC DNS RECORDS"
+                                        sleep 0.2
+                                        continue
+                                fi
+                        done
+                else
+                        lines
+                        echo "$file found"
+                        echo "Unable to identify the file path"
+                fi
         else
                 lines
-                echo "$filepath --> Unable to find"
+                echo "$file --> Not found"
+                echo "Invalid file path"
         fi
 fi
 rm -rf ./1
