96aa98267f7d940133924ee3dfae50154bdef06f
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-23 15:02:37 +0530

    Create comparsion_operators.sh

---
+++ comparsion_operators.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+echo "Enter the second number"
+read second
+
+#Comparsion operators 
+#> --> greater than, >= greaer than equal to
+#< -- less than, <= less than equal to 
+# == --> eqaul to, != --> no equal to 
+
+if [ $first -gt $second ]
+then
+        echo "First number:$first is greater than Second number:$second"
+elif [ $first -lt $second ]
+then
+        echo "First number:$first is less than Second number:$second"
+elif [ $first -eq $second ]
+then
+        echo "First number:$first is equal to Second number:$second"
+fi
