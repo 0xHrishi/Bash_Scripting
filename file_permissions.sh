f5d3da7c740b26edf9ed6fa5ec0b559d376dc167
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-23 15:52:24 +0530

    Create file_permissions.sh

---
+++ file_permissions.sh
@@ -0,0 +1,5 @@
+#file permissions 
+#Can help user to identify the file permissions on a file 
+-r --> File has read permissions
+-w --> file has write permissions 
+-x --> file has execute permissions
