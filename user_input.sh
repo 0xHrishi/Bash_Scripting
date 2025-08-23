128ba8b54a9a10489a86d9ed5b6be0cd115a8ca1
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-23 14:29:10 +0530

    Create user_input.sh

---
+++ user_input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#echo command to display the text
+echo "Enter your full name"
+#read command to wait for the user input
+read full_name
+
+echo "Enter your age"
+read age
+
+echo "Your full name --> $full_name and age is $age"
