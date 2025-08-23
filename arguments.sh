c4f5e70fe8f4ac29cfa5eef5009bc30607e9920f
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-23 15:54:42 +0530

    Create arguments.sh

---
+++ arguments.sh
@@ -0,0 +1,8 @@
+#Different types of arguments 
+
+$1 to $9 --> Arguments 
+$10 -- $n --> Arguments 
+$0 --> Name of the script 
+$? --> Exit status of the previous command 
+$# --> Total number of arguments 
+$@ --> Display all the arguments 
