e5ab027a078d40bde5b61177927365f3b6cb3d4f
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-09-20 15:19:22 +0530

    Create global_vs_local.sh

---
+++ global_vs_local.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Example for global and local variable
+#Global variable can be used within as well as outside the function
+#local variable is cerated within the function with the local keyword and can be used within the function only 
+
+#variable created 
+number=10
+
+#function created and within function a variable is created
+# Now the variable name within the function is same as the previous, so it will be a gloabl variable 
+function numbers {
+        number=20
+}
+
+echo "Number before the function called --> $number"
+numbers
+echo "Number after the function called --> $number"
+
+#Output
+#Number before the function called --> 10
+#Number after the function called --> 20
+
+echo "*********************************************************************************"
+sleep 2
+
+#Variable created
+num=10
+
+#function created and within the function a local keyword is created with the local keyword
+function nums {
+        local num=20
+        echo "Number wihtin the function --> $num"
+}
+
+echo "Number before the function called --> $num"
+nums
+echo "Number after the function called --> $num"
+
+#output
+#Number before the function called --> 10
+#Number wihtin the function --> 20
+#Number after the function called --> 10
+
+
