7e71d517b04fb51eb4911722257a70058d074ad4
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-10-10 11:06:28 +0530

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#loops are used for to perform repetive actions 
-
+#loops are used for to perform repetative actions
 
 number_1=1
 number_2=10
 
-#the loop will keep on executing until the last value is assigned to variable i.e. numbers
 #user create a variable i.e. numbers and there will be list of items.
 #When it comes to execution, first item from the listwill get assigned to the variable and loop will execute the code between do and done
 for numbers in $(seq $number_1 $number_2)
