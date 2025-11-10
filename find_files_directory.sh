e7754e427668e50c300f821f459004d09b2a9ce1
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-11-10 16:07:10 +0530

    Update find_files_directory.sh

--- find_files_directory.sh
+++ find_files_directory.sh
@@ -1,35 +1,49 @@
-#script takes user input as path to start, look for regular files or directories, modified in last how many days  
-
 #!/bin/bash
 
-echo "Enter the Path i.e. start searching files from"
-read path
+# Find files in a given path modified within a specific number of days
 
-echo "Would you like to find files or Directory i.e. f or d"
-read find
+function lines {
+        echo "******************************************************************************"
+}
 
-echo "Enter the modified number of days"
-read days
+# Function to check the number of days field must contain only numeric values
+function days_check {
+        [[ $days =~ ^[0-9-]+$ ]]
+}
 
-if [ -z "$path" ] ||  [ -z "$find" ] || [ -z "$days" ]
+#user input
+read -p "Enter the path: " filepath
+read -p "Enter the number of days: " days
+
+# Check if the user left any field empty
+if [ -z "$filepath" ] || [ -z "$days" ]
 then
-        if [ -z "$path" ] && ( [ -n "$find" ] && [ -n "$days" ] )
+        if [ -n "$filepath" ] && [ -z "$days" ]
         then
-                echo "Path field empty"
-        elif [ -z "$find" ] && ( [ -n "$path" ] && [ -n "$days" ] )
+                lines
+                echo "User input --> Number of days is empty"
+        elif [ -z "$filepath" ] && [ -n "$days" ]
         then
-                echo "Find field empty"
-        elif [ -z "$days" ] && ( [ -n "$path" ] && [ -n "$find" ] )
+                lines
+                echo "User input --> Fileapth field is empty"
+        elif [ -z "$filepath" ] && [ -z "$days" ]
         then
-                echo "Days field empty"
-        else
-                echo "Something wrong with user input"
+                lines
+                echo "User input --> Fileapth field is empty"
+                echo "User input --> Number of days is empty"
         fi
 
-elif [ -n "$path" ] && [ -n "$find" ] && [ -n "$days" ]
+# Both inputs are provided, validate 'days' field
+# Number of days field, check it must contain only numeric values
+
+elif [ -n "$filepath" ] && [ -n "$days" ]
 then
-        echo "Looking for $find, modified in last $days starting from $path directory path"
-        find $path -type $find -mtime -$days 2>/dev/null 1>output
-        cat output
+        if ! days_check
+        then
+                lines
+                echo "Number of days field must contain only numeric values"
+        elif days_check
+        then
+                find $filepath -mtime $days -exec ls -l {} \; 2>/dev/null
+        fi
 fi
-rm -rf ./output
