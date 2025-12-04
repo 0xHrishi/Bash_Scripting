f6625d823a72acd9044eb37afcf0db289f5cd25a
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-04 11:39:37 +0530

    Update 05_user_input.sh

--- bash_scripts/05_user_input.sh
+++ bash_scripts/05_user_input.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-#echo command to display the text
+# Prompt the user for their full name
 echo "Enter your full name"
-#read command to wait for the user input
+# Stores the entered name in the variable 'full_name'
 read full_name
 
+# Prompt the user for their age
 echo "Enter your age"
+# Stores the entered age in the variable 'age'
 read age
 
+# Display the collected information
 echo "Your full name --> $full_name and age is $age"
