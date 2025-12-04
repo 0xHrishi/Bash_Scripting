f6c9fdffd03f2456fb210aa68cfc8f52e4b0ae14
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-04 11:30:58 +0530

    Update 02_declaring_variables.sh

--- bash_scripts/02_declaring_variables.sh
+++ bash_scripts/02_declaring_variables.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-#Variables allows to store data temporaily
+# --------------------------------------------
+# Basic Bash Script Demonstration -- Shows variables, command substitution, and system information fetching.
+# --------------------------------------------
 
-#Variable created i.e. first_number
-#with the help of echo command, displayed the value for variable by using $ symbol
-first_number=10
-echo $first_number
-echo "*****************************************************************"
+# Integer variable
+a=10
+echo "Number is $a"
 
-#variable full_name which stores user full name 
-#In bash scripting, there is no data type concept, so the values are stored as string by default 
-full_name="Hrishi Dilip Ghosalkar"
-echo $full_name
-echo "*****************************************************************"
+# String variable
+tv_show="Harry Potter"
+echo "Favourite television show -- $tv_show"
 
-#command substitution
-#Assign output of a command as a value to the variable
-date_output=$(date)
-echo $date_output
-echo "*****************************************************************"
+# Store current date and time using command substitution
+current=$(date)
+echo "Current date and time is $current"
 
-#File output to a variable as value 
-file_output=$(cat /etc/resolv.conf | head -n 1)
-echo $file_output
-echo "*****************************************************************"
+# Print the current working directory
+echo "Current working directory -- $(pwd)"
 
-#system defined variable i.e. $USER which displays the current login user
-echo "Hello $USER, todays date and time is: $(date)"
-echo "*****************************************************************"
-
+# Print the current user's UID using id command
+echo "Current login user UID number -- $(id -u)"
