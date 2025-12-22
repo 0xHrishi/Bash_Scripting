264577d06fabe62fc549acfb5d4f927a4af29fc8
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-22 16:52:37 +0530

    Update 05_user_input.sh

--- bash_scripts/05_user_input.sh
+++ bash_scripts/05_user_input.sh
@@ -12,3 +12,8 @@ read age
 
 # Display the collected information
 echo "Your full name --> $full_name and age is $age"
+
+# With read command 
+read -p "Enter the username: " username
+read -sp "Enter the password: " password
+echo "Username is $username and password is $password"
