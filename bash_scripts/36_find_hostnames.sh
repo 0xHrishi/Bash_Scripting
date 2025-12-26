db68f8293043b9d165d0db1b30cd21afd35cc558
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-26 10:53:46 +0530

    Update 36_find_hostnames.sh

--- bash_scripts/36_find_hostnames.sh
+++ bash_scripts/36_find_hostnames.sh
@@ -1,71 +1,63 @@
 #!/bin/bash
+# Brute force attack on the domain name, to find different hostname
 
-# This script takes a domain name (e.g., abc.com) and a file path that contains
-# hostnames (one per line).
 function lines {
-        echo "*************************************************************"
-
+        echo "****************************************************************"
 }
 
-# Prompt user input
-read -p "Enter the domain name i.e. abc.com: " domain_name
-read -p "Enter the file path: " filepath
+# color codes for error messages
+red='\033[0;31m'
+nf='\033[0m'
 
-# User input empty
-if [ -z "$domain_name" ] || [ -z "$filepath" ]
+# User input
+read -p "Enter the domain example: abc.com: " domain_name
+read -p "Enter the file path for brute force: " file
+
+# User input -- empty
+if [ -z "$domain_name" ] || [ -z "$file" ]
 then
-        if [ -n "$domain_name" ] && [ -z "$filepath" ]
+        lines
+        if [ -z "$domain_name" ]
         then
-                lines
-                echo "User input --> Filepath field is empty"
-        elif [ -z "$domain_name" ] && [ -n "$filepath" ]
-        then
-                lines
-                echo "User input --> Domain name field is empty"
-        elif [ -z "$domain_name" ] && [ -z "$filepath" ]
-        then
-                lines
-                echo "User input --> Domain name field is empty"
-                echo "User input --> Filepath field is empty"
+                echo -e "${red}User input --> Domain name field is empty${nf}"
         fi
-
-# Proceed when both domain name and file path are provided
-# Checks whether the file exists, is readable, and is not empty.
-
-
-elif [ -n "$domain_name" ] && [ -n "$filepath" ]
-then
-        if [ -e $filepath ]
+        if [ -z "$file" ]
         then
-                if [ -f $filepath ] && [ -s $filepath ]
+                echo -e "${red}User input --> Filepath field is empty${nf}"
+        fi
+# If both inputs are provided
+# Check if the file exists
+# If input path is a non-empty regular file, Loop through each subdomain in the wordlist
+elif [ -n "$domain_name" ] && [ -n "$file" ]
+then
+        if [ -e "$file" ]
+        then
+                lines
+                if [ -d "$file" ]
                 then
-                        for hostname in $(cat $filepath)
+                        echo -e "${red}$file --> Its a directory${nf}"
+                elif [ -f "$file" ] && [ -s "$file" ]
+                then
+                        for hostnames in $(cat $file)
                         do
-                                host $hostname.$domain_name &>/dev/null
+                                lines
+                                host $hostnames.$domain_name &>/dev/null
 
                                 if [ $? -eq 0 ]
                                 then
-                                        lines
-                                        host $hostname.$domain_name
+                                        host $hostnames.$domain_name
                                         sleep 0.2
                                 else
-                                        lines
-                                        echo "$hostname.$domain_name --> NO PUBLIC DNS RECORDS"
+                                        echo "$hostnames.$domain_name --> No public DNS records"
                                         sleep 0.2
-                                        continue
                                 fi
                         done
 
-                elif [ -d $filepath ]
-                then
-                        lines
-                        echo "$filepath --> Its a directory"
                 else
-                        lines
-                        echo "$filepath --> Found, but unable to identify the file type"
+                        echo -e "${red}File found -- Unable to identify the file type"
                 fi
         else
                 lines
-                echo "$filepath --> Not found"
+                echo -e "${red}$file -- Not found${nf}"
         fi
 fi
