69df84d6fb158e00142b9c42d6ed198cbda02161
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-26 10:08:10 +0530

    Update 32_oscp_zone_file.sh

--- bash_scripts/32_oscp_zone_file.sh
+++ bash_scripts/32_oscp_zone_file.sh
@@ -1,60 +1,58 @@
 #!/bin/bash
-
-# Checks domain reachability, lists name servers, and optionally attempts a DNS zone transfer.
-
+# Performs DNS enumeration and attempts zone transfer (AXFR) on a given domain.
 function lines {
-        echo "**************************************************"
+        echo "****************************************************************"
 }
 
-# Prompt user for domain name
-read -p "Enter the domain name example abc.com: " domain_name
+# color code for error messages
+red='\033[0;31m'
+nf='\033[0m'
 
-# Check if domain name is empty
+# Prompt userinput
+read -p "Enter the domain name i.e. www.abc.com: " domain_name
+
+# id user input empty
 if [ -z "$domain_name" ]
 then
         lines
-        echo "User input -- Domain name field is empty"
-
-# Domain name is not empty
-# Test if the domain resolves (silently discard output)
-# Save nameservers to a temporary file
-# Attempt zone transfer for each nameserver
- 
+        echo -e "${red}User input -- Domain name field is empty${nf}"
+# User input not empty
+# Check whether the domain has public DNS records
+# Display DNS information for the domain and display the dns servers 
+# Save name servers to a file for zone transfer attempts
+# Attempt zone file transfer
 elif [ -n "$domain_name" ]
 then
-        host www.$domain_name &>/dev/null
+        lines
+        host $domain_name &>/dev/null
 
         if [ $? -eq 0 ]
         then
-                echo "***************** Name Servers ********************"
+                host $domain_name
+                lines
+                echo -e "${red}Looking for DNS Servers${nf}"
+                sleep 0.2
+                domain_name=$(echo "$domain_name" | cut -d "." -f2,3)
                 host -t ns $domain_name | cut -d " " -f4
+                host -t ns $domain_name | cut -d " " -f4 > dns_servers
                 lines
-                echo "Would you like to do a zone transfer y/n ?"
-                read option
+                echo -e "${red}Zone file transfer${nf}"
 
-                if [ "$option" == "y" ]
-                then
-                        host -t ns $domain_name | cut -d " " -f4 > ./name_servers
-                        for zone_transfer in $(cat ./name_servers)
-                        do
-                                lines
+                lines
+                for zone_transfer in $(cat dns_servers)
+                do
+                        host -l $domain_name $zone_transfer &>/dev/null
+
+                        if [ $? -eq 0 ]
+                        then
                                 host -l $domain_name $zone_transfer
-
-                        done
-
-                elif [ "$option" == "n" ]
-                then
-                        lines
-                        echo "You choose to quit"
-                else
-                        lines
-                        echo "Invalid user input"
-                fi
+                        else
+                                continue
+                        fi
+                done
         else
-                lines
-                echo "Unable to connect -- $domain_name"
+                echo "$domain_name --> No public DNS records"
+
         fi
 fi
-
-# Clean up temporary file
-rm -rf ./name_servers
+rm -rf ./dns_servers
