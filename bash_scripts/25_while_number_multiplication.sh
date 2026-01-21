b2ba7d2b11d2d7ff9472ebf7cfd40b5e2a467b56
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-01-21 17:31:37 +0530

    Update 25_while_number_multiplication.sh

--- bash_scripts/25_while_number_multiplication.sh
+++ bash_scripts/25_while_number_multiplication.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-# Performs multiplication using a multiplicand and multiplier
-# and continues multiplying until the result exceeds a stop number.
+
+# Description : Multiplies a multiplicand and multiplier.
+#               If the result is less than the stop number,
+#               the multiplier is incremented until the
+#               result reaches or exceeds the stop number.
 
 function lines {
-        echo "****************************************************************************"
+        echo "****************************************************************"
 }
 
-# color codes for error messages
-red='\033[0;31m'
-nf='\033[0m'
+# color codes for error message
+RED='\033[0;31m'
+NF='\033[0m'
 
-# Function to make sure only numeric values are expected
+# function to validate the user input
+# only positive numeric values allowed
+# no zero, no leading zero
 function multiplicand_check {
         [[ $multiplicand =~ ^[1-9][0-9]*$ ]]
 }
@@ -21,36 +26,36 @@ function stop_number_check {
         [[ $stop_number =~ ^[1-9][0-9]*$ ]]
 }
 
-echo "User input must not start with zero i.e. 0, 01, 001 .."
-sleep 0.2 
-# Prompt user input
+# User instruction
+echo -e "${RED}Do not enter numbers with leading zero or negative numbers${NF}"
+sleep 0.2
+# Prompt for user input
 read -p "Enter the multiplicand number: " multiplicand
 read -p "Enter the multiplier number: " multiplier
 read -p "Enter the stop number: " stop_number
 
-# User input empty
+# user input empty
 if [ -z "$multiplicand" ] || [ -z "$multiplier" ] || [ -z "$stop_number" ]
 then
         lines
         if [ -z "$multiplicand" ]
         then
-                echo -e "${red}User input -- Multiplicand number field is empty${nf}"
+                echo -e "${RED}User input --> Multiplicand number field is empty${NF}"
         fi
         if [ -z "$multiplier" ]
         then
-                echo -e "${red}User input -- multiplier number field is empty${nf}"
+                echo -e "${RED}User input --> Multiplier number field is empty${NF}"
         fi
         if [ -z "$stop_number" ]
         then
-                echo -e "${red}User input -- Stop number field is empty${nf}"
+                echo -e "${RED}User input --> Stop number field is empty${NF}"
         fi
 
-# User input not empty
-# Validate the user input i.e. check whether they are numeric or not
-# If numeric, 
-# Case 1: Result equals stop number
-# Case 2: Result already greater than stop number
-# Case 3: Keep multiplying until result exceeds stop number
+# user input not empty
+# validate the user input 
+# Multiplication i.e. multiplicand * multiplier number
+# compare the result with stop number
+# increment the multiplier 
 elif [ -n "$multiplicand" ] && [ -n "$multiplier" ] && [ -n "$stop_number" ]
 then
         if ! multiplicand_check || ! multiplier_check || ! stop_number_check
@@ -58,40 +63,39 @@ then
                 lines
                 if ! multiplicand_check
                 then
-                        echo -e "${red}Multiplicand number field must contain only numeric values ${nf}"
+                        echo -e "${RED}Multiplicand number must contain only positive integers${NF}"
                 fi
                 if ! multiplier_check
                 then
-                        echo -e "${red}Multiplier number field must contain only numeric values ${nf}"
+                        echo -e "${RED}Multiplier number must contain only positive integers${NF}"
                 fi
                 if ! stop_number_check
                 then
-                        echo -e "${red}Stop number field must contain only numeric values ${nf}"
+                        echo -e "${RED}Stop number must contain only positive integers${NF}"
                 fi
+
         elif multiplicand_check && multiplier_check && stop_number_check
         then
                 lines
-                multiply_result=$(($multiplicand*$multiplier))
-
-                if [ $multiply_result -eq $stop_number ]
+                result=$(($multiplicand*$multiplier))
+                if [ $result -eq $stop_number ]
                 then
-                        echo "Multiplicand: $multiplicand times $multiplier --> $multiply_result"
-                        echo "Stop number --> $stop_number"
-                        echo "Both number are equal"
-                elif [ $multiply_result -gt $stop_number ]
+                        echo -e "${RED}Multiplicand number: $multiplicand times Multiplier: $multiplier --> $result${NF}"
+                        echo -e "${RED}Result: $result is equal to Stop number: $stop_number${NF}"
+                elif [ $result -gt $stop_number ]
                 then
-                        echo "Multiplicand: $multiplicand times $multiplier --> $multiply_result"
-                        echo "Stop number --> $stop_number"
-                        echo "Multiply result: $multiply_result is greater than $stop_number"
-                elif [ $multiply_result -le $stop_number ]
+                        echo -e "${RED}Multiplicand number: $multiplicand times Multiplier: $multiplier --> $result${NF}"
+                        echo -e "${RED}Result: $result is greater than Stop number: $stop_number${NF}"
+                elif [ $result -lt $stop_number ]
                 then
-                        while [ $multiply_result -le $stop_number ]
+                        while [ $result -lt $stop_number ]
                         do
-                                echo "Multiplicand: $multiplicand times $multiplier --> $multiply_result"
+                                echo -e "${RED}Multiplicand number: $multiplicand times Multiplier: $multiplier --> $result${NF}"
                                 ((multiplier++))
-                                multiply_result=$(($multiplicand*$multiplier))
+                                result=$(($multiplicand*$multiplier))
                                 sleep 0.2
                         done
                 fi
+
         fi
 fi
