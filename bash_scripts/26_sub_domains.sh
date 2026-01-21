cb3be534f8fb440ce0691a552d4147dbfa97b15d
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-01-21 18:04:16 +0530

    Update 26_sub_domains.sh

--- bash_scripts/26_sub_domains.sh
+++ bash_scripts/26_sub_domains.sh
@@ -1,65 +1,68 @@
 #!/bin/bash
-# Performs subdomain brute-forcing using a wordlist
-# and checks for public DNS records using the `host` command.
 
+# brute force attack on a domain to find sub domains 
 function lines {
-        echo "****************************************************************************"
+        echo "****************************************************************"
 }
 
-# color code for error messages
-red='\033[0;31m'
-nf='\033[0m'
+# color codes for error messages
+RED='\033[0;31m'
+NF='\033[0m'
 
-# Read user input
-read -p "Enter the domain name: " domain_name
-read -p "Enter the filepath for brute force: " filepath
+# user instructions
+echo -e "${RED}Brute force attack on domain to find sub domains${NF}"
+sleep 0.2
+# prompt for user input
+read -p "Enter the domain, example as abc.com: " domain_name
+read -p "Enter the file path which consists list of hostnames: " file
 
-# User input empty
-if [ -z "$domain_name" ] || [ -z "$filepath" ]
+# user input empty
+if [ -z "$domain_name" ] || [ -z "$file" ]
 then
         lines
         if [ -z "$domain_name" ]
         then
-                echo -e "${red}User input -- Domain name field is empty${nf}"
+                echo -e "${RED}User input --> Domain name field is empty${NF}"
         fi
-        if [ -z "$file_path" ]
+        if [ -z "$file" ]
         then
-                echo -e "${red}User input -- Filepath field is empty${nf}"
+                echo -e "${RED}User input --> Filepath field is empty${NF}"
         fi
 
-# User input not empty
-# Valid regular file and its not empty, 
-# Read each subdomain from the wordlist
-# Suppress output, check only exit status
-elif [ -n "$domain_name" ] && [ -n "$filepath" ]
+# user input is not empty
+# Check if file exists, Suppress output and check DNS resolution
+elif [ -n "$domain_name" ] && [ -n "$file" ]
 then
-        if [ -e $filepath ]
+        lines
+        if [ -e $file ]
         then
-                lines
-                if [ -d $filepath ]
+                if [ -d $file ]
                 then
-                        echo "$filepath --> Its a directory"
-                elif [ -f $filepath ] && [ -s $filepath ]
+                        echo -e "${RED}File: $file is a directory${NF}"
+                elif [ -f $file ] && [ -s $file ]
                 then
-                        for sub_domains in $(cat $filepath)
+                        while read -r sub_domains
                         do
-                                lines
                                 host $sub_domains.$domain_name &>/dev/null
 
                                 if [ $? -eq 0 ]
                                 then
-                                        host $sub_domains.$domain_name
-                                        sleep 0.2
+                                        output=$(host $sub_domains.$domain_name)
+                                        echo -e "${RED}$output${NF}"
+                                        sleep 0.5
+                                        lines
                                 else
-                                        echo "$sub_domains.$domain_name --> No public dns records"
-                                        sleep 0.2
-                                        continue
+                                        echo -e "$sub_domains.$domain_name --> No DNS records found"
+                                        sleep 0.5
+                                        lines
                                 fi
 
-                        done
-
+                        done < $file
                 else
-                        echo "$filepath --> File found, but unable to identify the filetype"
+                        echo -e "${RED}File: $file found, unable to identify the file type${NF}"
                 fi
+        else
+                echo -e "${RED}File: $file not found${NF}"
+
         fi
 fi
