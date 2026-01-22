a6752a1e5f08f64998b4b1f66119ccfc0c0669f1
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-01-22 12:20:59 +0530

    Update 27_calculations_with_case.sh

--- bash_scripts/27_calculations_with_case.sh
+++ bash_scripts/27_calculations_with_case.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
-# Arithmetic operators with case statements 
+
+# Description: This script accepts two positive integers (no leading zeros, no decimals)
+#              and allows the user to perform basic arithmetic operations:
+#              Addition, Subtraction, Multiplication, Power, Quotient, and Remainder.
+
 function lines {
-        echo "****************************************************************"
+        echo "***************************************************************"
 }
 
 # color codes for error
-red='\033[0;31m'
-nf='\033[0m'
+RED='\033[0;31m'
+NF='\033[0m'
 
-# Function to make sure, the user input contain only numeric values
+# Function to check the user input must contain only numeric values
 function first_number_check {
-        [[ $first_number =~ ^[1-9-][0-9]*$ ]]
+        [[ $first_number =~ ^[1-9][0-9]*$ ]]
 }
 function second_number_check {
-        [[ $second_number =~ ^[1-9-][0-9]*$ ]]
+        [[ $second_number =~ ^[1-9][0-9]*$ ]]
 }
 
-# Prompt user, not to provide input with leading 0
-echo "*****User input must not start with zero i.e. 0, 01, 001"
+# User input instructions
+echo -e "${RED}Do not enter numbers leading with 0 or 0 or decimal values${NF}"
 sleep 0.2
-# User input
+# user input
 read -p "Enter the first number: " first_number
 read -p "Enter the second number: " second_number
 
-# User input empty
+# user input empty
 if [ -z "$first_number" ] || [ -z "$second_number" ]
 then
         lines
         if [ -z "$first_number" ]
         then
-                echo -e "${red}User input --> First number field is empty${nf}"
+                echo -e "${RED}User input --> First number field is empty"
         fi
         if [ -z "$second_number" ]
         then
-                echo -e "${red}User input --> Second number field is empty${nf}"
+                echo -e "${RED}User input --> Second number field is empty"
         fi
-
 # user input not empty
-# Validation check -- Make sure the user input contain only numeric values
-# Validation pass -- arithmetic operators with case statements
+# validation check
+# validation passes --  If both numbers are valid, show menu
+# Perform operation based on user choice
 elif [ -n "$first_number" ] && [ -n "$second_number" ]
 then
         if ! first_number_check || ! second_number_check
@@ -46,35 +50,34 @@ then
                 lines
                 if ! first_number_check
                 then
-                        echo -e "${red}First number field must contain numeric values i.e. positive or negative${nf}"
+                        echo -e "${RED}First number field must contain only numeric values${NF}"
                 fi
                 if ! second_number_check
                 then
-                        echo -e "${red}Second number field must contain numeric values i.e. positive or negative${nf}"
+                        echo -e "${RED}Second number field must contain only numeric values${NF}"
                 fi
         elif first_number_check && second_number_check
         then
                 lines
-                echo "Press 1 -- Addition"
-                echo "Press 2-- Subtraction"
-                echo "Press 3 -- Multiply"
-                echo "Press 4 -- Power"
-                echo "Press 5 -- Quotient"
-                echo "Press 6 -- Remainder"
-                read option
-
+                echo "Press 1 for Addition"
+                echo "Press 2 for Subtraction"
+                echo "Press 3 for Multiplication"
+                echo "Press 4 for Power"
+                echo "Press 5 for Quotient"
+                echo "Press 6 for Remainder"
+                read choice
                 lines
-                case $option in
+                case $choice in
                         1)
-                                sum=$(($first_number+$second_number))
-                                echo "First number: $first_number plus Second number: $second_number --> $sum"
+                                add=$(($first_number+$second_number))
+                                echo "First number: $first_number plus Second number: $second_number --> $add"
                                 ;;
                         2)
-                                subtract=$(($first_number-$second_number))
-                                echo "First number: $first_number minus Second number: $second_number --> $subtract"
+                                sub=$(($first_number-$second_number))
+                                echo "First number: $first_number subtract Second number: $second_number --> $sub"
                                 ;;
                         3)
-                                multiply=$(($first_number*second_number))
+                                multiply=$(($first_number*$second_number))
                                 echo "First number: $first_number multiply Second number: $second_number --> $multiply"
                                 ;;
                         4)
@@ -90,8 +93,7 @@ then
                                 echo "First number: $first_number divide (Remainder) Second number: $second_number --> $remainder"
                                 ;;
                         *)
-                                echo "Inavlid user input"
-                                ;;
+                                echo -e "${RED}Invalid user input${NF}"
                 esac
         fi
 fi
