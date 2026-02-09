fdf6afb5c997c8b078f2b7001aa09b9a84b380b4
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-09 16:01:34 +0530

    Update 26_sub_domains.sh

--- bash_scripts/26_sub_domains.sh
+++ bash_scripts/26_sub_domains.sh
@@ -1,68 +1,63 @@
 #!/bin/bash
-
-# brute force attack on a domain to find sub domains 
+# Description : This script performs subdomain brute-forcing on a domain using a wordlist
 function lines {
-        echo "****************************************************************"
+        echo "*****************************************************"
 }
 
-# color codes for error messages
-RED='\033[0;31m'
+# color codes for error messafes
+RED='\033[1;31m'
 NF='\033[0m'
 
-# user instructions
-echo -e "${RED}Brute force attack on domain to find sub domains${NF}"
-sleep 0.2
-# prompt for user input
-read -p "Enter the domain, example as abc.com: " domain_name
-read -p "Enter the file path which consists list of hostnames: " file
+# User input
+read -p "Enter the domain name: " domain_name
+read -p "Enter the wordlists filepath for brute force attack: " filepath
 
 # user input empty
-if [ -z "$domain_name" ] || [ -z "$file" ]
+if [ -z "$domain_name" ] || [ -z "$filepath" ]
 then
         lines
         if [ -z "$domain_name" ]
         then
                 echo -e "${RED}User input --> Domain name field is empty${NF}"
         fi
-        if [ -z "$file" ]
+        if [ -z "$filepath" ]
         then
                 echo -e "${RED}User input --> Filepath field is empty${NF}"
         fi
-
-# user input is not empty
-# Check if file exists, Suppress output and check DNS resolution
-elif [ -n "$domain_name" ] && [ -n "$file" ]
-then
+# user input not empty
+# Check if file path exists
+# regular file exist, Perform DNS lookup and display result
+else
         lines
-        if [ -e $file ]
+        if [ -e "$filepath" ]
         then
-                if [ -d $file ]
+                if [ -d "$filepath" ]
                 then
-                        echo -e "${RED}File: $file is a directory${NF}"
-                elif [ -f $file ] && [ -s $file ]
+                        echo -e "${RED}File: $filepath, its a directory${NF}"
+                elif [ -f "$filepath" ] && [ -s "$filepath" ]
                 then
-                        while read -r sub_domains
+                        for sub_domains in $(cat $filepath)
                         do
+                                lines
                                 host $sub_domains.$domain_name &>/dev/null
 
                                 if [ $? -eq 0 ]
                                 then
-                                        output=$(host $sub_domains.$domain_name)
-                                        echo -e "${RED}$output${NF}"
-                                        sleep 0.5
-                                        lines
+                                        echo -e "${RED}$(host $sub_domains.$domain_name)${NF}"
+                                        sleep 0.2
                                 else
-                                        echo -e "$sub_domains.$domain_name --> No DNS records found"
-                                        sleep 0.5
-                                        lines
+                                        echo "$sub_domains.$domain_name --> NO DNS RECORDS"
+                                        sleep 0.2
+
                                 fi
 
-                        done < $file
+                        done
                 else
-                        echo -e "${RED}File: $file found, unable to identify the file type${NF}"
+                        echo -e "${RED}File: $filepath, Unable to identify the file type${NF}"
+
                 fi
         else
-                echo -e "${RED}File: $file not found${NF}"
+                echo -e "${RED}File: $filepath, Invalid filepath${NF}"
 
         fi
 fi
