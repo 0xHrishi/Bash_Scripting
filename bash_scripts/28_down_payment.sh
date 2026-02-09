091cf1c1942d553a76392ae034ed64358cbbd5f3
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-09 17:26:35 +0530

    Update 28_down_payment.sh

--- bash_scripts/28_down_payment.sh
+++ bash_scripts/28_down_payment.sh
@@ -17,81 +17,75 @@ function lines {
         echo "***************************************************************"
 }
 
-# color codes for error messages
-RED='\033[0;31m'
+# color codes for error
+RED='\033[1;31m'
 NF='\033[0m'
 
-# function to validate the user input
-# Function: Validate positive integers with no leading zeros
+# function to validate user input i.e. only integer valuies accepted
+# - Rejects zero and numbers starting with zero
 function total_cost_check {
         [[ $total_cost =~ ^[1-9][0-9]*$ ]]
 }
 function credit_score_check {
         [[ $credit_score =~ ^[1-9][0-9]*$ ]]
 }
-
-# user instructions
-echo -e "${RED}Do not enter input leading with zero${NF}"
-sleep 0.2
-# User input
+#warning
+echo "Do not enter user input starting with zero or zero"
+echo
+# user input
 read -p "Enter the total cost of the house: " total_cost
 read -p "Enter the credit score: " credit_score
 
-# user input empty
+# user iput empty
 if [ -z "$total_cost" ] || [ -z "$credit_score" ]
 then
         lines
         if [ -z "$total_cost" ]
         then
-                echo -e "${RED}User input --> Total cost field is empty"
+                echo -e "${RED}User input --> Total cost of the house field is empty${NF}"
         fi
         if [ -z "$credit_score" ]
         then
-                echo -e "${RED}User input --> Credit score field is empty"
+                echo -e "${RED}User input --> Credit score field is empty${NF}"
         fi
-# User input provided
-# valiudation check
-# validation pass -- Calculate down payment based on credit score
-elif [ -n "$total_cost" ] && [ -n "$credit_score" ]
-then
+# user input not empty
+# user input check,
+# validation passed, caluclate the min down payment based upon the total cost and credit score
+else
+        lines
         if ! total_cost_check || ! credit_score_check
         then
-                lines
                 if ! total_cost_check
                 then
-                        echo -e "${RED}Total cost of the house field must contain onyl numeric values${NF}"
+                        echo -e "${RED}Total cost of the house field must contain only integer values${NF}"
                 fi
                 if ! credit_score_check
                 then
-                        echo -e "${RED}Credit score field must contain onyl numeric values${NF}"
+                        echo -e "${RED}Credit score field must contain only integer values${NF}"
                 fi
-        elif total_cost_check && credit_score_check
-        then
-                lines
+        else
                 if [ $credit_score -gt 999 ]
                 then
-                        echo -e "${RED}Credit score cannot be greater than 999${NF}"
-                fi
-                if [ $credit_score -le 999 ]
-                then
+                        echo -e "${RED}Credit score cannot be greater than 999"
+                else
                         echo "Total cost of the house --> $total_cost"
                         echo "Credit score --> $credit_score"
                         if [ $credit_score -ge 750 ]
                         then
                                 down_payment=$(($total_cost*10/100))
-                                echo "Down payment --> $down_payment"
-                        elif [ $credit_score -ge 650 ] && [ $credit_score -le 749 ]
+                                echo "Minimum Down payment --> $down_payment"
+                        elif [ $credit_score -ge 650 ] && [ $credit_score -lt 750 ]
                         then
                                 down_payment=$(($total_cost*20/100))
-                                echo "Down payment --> $down_payment"
-                        elif [ $credit_score -ge 600 ] && [ $credit_score -le 649 ]
+                                echo "Minimum Down payment --> $down_payment"
+                        elif [ $credit_score -ge 600 ] && [ $credit_score -lt 650 ]
                         then
                                 down_payment=$(($total_cost*30/100))
-                                echo "Down payment --> $down_payment"
+                                echo "Minimum Down payment --> $down_payment"
                         else
                                 echo -e "${RED}Cannot proceed with the application${NF}"
                         fi
                 fi
-        fi
 
+        fi
 fi
