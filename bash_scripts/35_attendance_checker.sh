ddcb7604db9e97b7f4fbd055e695523103ab3b16
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-12 18:50:30 +0530

    Update 35_attendance_checker.sh

--- bash_scripts/35_attendance_checker.sh
+++ bash_scripts/35_attendance_checker.sh
@@ -67,3 +67,69 @@ else
                 fi
         fi
 fi
+--------------------------------------------------------------------------------------------------------
+ADVANCED VERSION
+
+
+function lines {
+        echo "********************************************************"
+}
+
+RED='\033[1;31m'
+NF='\033[0m'
+function total_classes_check {
+        [[ $total_classes =~ ^[1-9][0-9]*$ ]]
+}
+function attended_classes_check {
+        [[ $attended_classes =~ ^[1-9][0-9]*$ ]]
+}
+read -p "Enter the total number of classes: " total_classes
+read -p "Enter the total number of classes attended: " attended_classes
+
+
+if [ -z "$total_classes" ] || [ -z "$attended_classes" ]
+then
+        lines
+        if [ -z "$total_classes" ]
+        then
+                echo -e "${RED}User input --> Total number of classes field is empty${NF}"
+        fi
+        if [ -z "$attended_classes" ]
+        then
+                echo -e "${RED}User input --> Total number of classes attended field is empty${NF}"
+        fi
+
+else
+        lines
+        if ! total_classes_check || ! attended_classes_check
+        then
+                if ! total_classes_check
+                then
+                        echo -e "${RED}Total number of classes field must contain only numeric values${NF}"
+                fi
+                if ! attended_classes_check
+                then
+                        echo -e "${RED}Total number of classes attended field must contain only numeric values${NF}"
+                fi
+        else
+                if [ $total_classes -lt $attended_classes ]
+                then
+                        echo -e "${RED}Total number of classes cannot be lesser than number of classes attended ${NF}"
+                else
+                        attendance=$(bc <<< "scale=2; $attended_classes * 100 / $total_classes")
+
+                        echo "Total number of classes --> $total_classes"
+                        echo "Total number of classes attended --> $attended_classes"
+                        if (( $(bc <<< "$attendance >= 75") ))
+                        then
+                                echo "Attendance percentage --> $attendance"
+                        elif (( $(bc <<< "$attendance >= 60") )) && (( $(bc <<< "$attendance <= 74") ))
+                        then
+                                echo "Attendance percentage --> $attendance"
+                                echo -e "${RED}CAREFUL${NF}"
+                        else
+                                echo -e "${RED}CONTACT PRINCIPAL${NF}"
+                        fi
+                fi
+        fi
+fi
