cde32f99ceb2e4dfd1a762282a8bacb02d33e42e
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-12 13:20:21 +0530

    Update 32_oscp_zone_file.sh

--- bash_scripts/32_oscp_zone_file.sh
+++ bash_scripts/32_oscp_zone_file.sh
@@ -1,58 +1,37 @@
 #!/bin/bash
-# Performs DNS enumeration and attempts zone transfer (AXFR) on a given domain.
+#   This script attempts a DNS Zone Transfer (AXFR) against
+#   the name servers of a given domain.
 function lines {
-        echo "****************************************************************"
+        echo "******************************************************"
 }
+# color codes for error
+RED='\033[1;31m'
+NF='\033[0m'
+# user input
+read -p "Enter the domain name such as abc.com: " domain_name
 
-# color code for error messages
-red='\033[0;31m'
-nf='\033[0m'
-
-# Prompt userinput
-read -p "Enter the domain name i.e. www.abc.com: " domain_name
-
-# id user input empty
+# user input empty
 if [ -z "$domain_name" ]
 then
         lines
-        echo -e "${red}User input -- Domain name field is empty${nf}"
-# User input not empty
-# Check whether the domain has public DNS records
-# Display DNS information for the domain and display the dns servers 
-# Save name servers to a file for zone transfer attempts
-# Attempt zone file transfer
-elif [ -n "$domain_name" ]
-then
-        lines
-        host $domain_name &>/dev/null
+        echo -e "${RED}User input --> Domain name field is empty${NF}"
+# user input not empty
+# Retrieve NS records and extract nameserver hostnames
+# Loop through each nameserver and attempt zone transfer
+ # If transfer succeeds, display full zone dat
+else
+        host -t ns $domain_name | cut -d " " -f4 1> name_servers
 
-        if [ $? -eq 0 ]
-        then
-                host $domain_name
-                lines
-                echo -e "${red}Looking for DNS Servers${nf}"
-                sleep 0.2
-                domain_name=$(echo "$domain_name" | cut -d "." -f2,3)
-                host -t ns $domain_name | cut -d " " -f4
-                host -t ns $domain_name | cut -d " " -f4 > dns_servers
-                lines
-                echo -e "${red}Zone file transfer${nf}"
+        echo "********Zone file transfer***************"
 
-                lines
-                for zone_transfer in $(cat dns_servers)
-                do
-                        host -l $domain_name $zone_transfer &>/dev/null
+        for zone_file_transfer in $(cat ./name_servers)
+        do
+                host -l "$domain_name" "$zone_file_transfer" &>/dev/null
 
-                        if [ $? -eq 0 ]
-                        then
-                                host -l $domain_name $zone_transfer
-                        else
-                                continue
-                        fi
-                done
-        else
-                echo "$domain_name --> No public DNS records"
-
-        fi
+                if [ $? -eq 0 ]
+                then
+                        host -l $domain_name $zone_file_transfer
+                fi
+        done
 fi
-rm -rf ./dns_servers
+rm -rf ./name_servers
